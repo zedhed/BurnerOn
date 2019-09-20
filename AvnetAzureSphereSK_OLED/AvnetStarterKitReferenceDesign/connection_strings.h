7b8d9907d2fa433ecf9d351c30c9111030607feb
Kevin Keryk <kevin.keryk@avnet.com>
2019-09-19 21:48:38 -0700

    Initial commit of code for BurnerOn project

---
+++ AvnetAzureSphereSK_OLED/AvnetStarterKitReferenceDesign/connection_strings.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Define your connection string here.  The connection string is required to connect to Azure.
+// #define MY_CONNECTION_STRING "HostName=iotc-xxxx....azure-devices.net;DeviceId=xxxx....;SharedAccessKey=xxxxx..."
+
+#define MY_CONNECTION_STRING "HostName=iotc-0ea275f5-a398-402d-8951-5e2898879965.azure-devices.net;DeviceId=63cd5f04-0505-4b65-9cdf-a270db359d6d;SharedAccessKey=amsMKOSmIqFAiV0uyJ5VPuvdOBqSplQEEVt29GsOH58="
